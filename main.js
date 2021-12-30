d3d95bbc195429d18cc2e827b52e481212d1038b
Barpotroszek <77773157+Barpotroszek@users.noreply.github.com>
2021-12-30 14:21:34 +0100

    Repaired some issues
    Repaired not working return button, removed all console logs and
    formated document

--- main.js
+++ main.js
@@ -53,7 +53,7 @@ function log(text) {
 function change_title(new_title) {
     function edit(elem, new_txt) {
         let txt = elem.innerHTML;
-        elem.innerHTML =  new_title + txt;
+        elem.innerHTML = new_title + txt;
     }
     edit(document.getElementsByTagName("title")[0], new_title);
     edit(document.getElementById("story-title"), new_title);
@@ -68,7 +68,7 @@ function slice_url() {
     }
 }
 
-const notFound = () =>{
+const notFound = () => {
     window.location.pathname = "404.html"; //jest id, nie ma daty -> błąd
 }
 
@@ -79,17 +79,16 @@ const categories = {
     "modlitwa": "Modlitwa",
     "poprawki": "Możliwe poprawki",
     "inne": "Inne",
-    "updates":"Aktualizacje",
-    "non-censored":"Niecenzuralne"
+    "updates": "Aktualizacje",
+    "non-censored": "Niecenzuralne"
 }
 
 function stuff_with_json_file(window_url) {
     // Ustawia link do ostatnio udostępnionego postu
     // i zwraca hiperłącza do wskazanych postów(jeśli set_hrefs == true)
     const xhr = new XMLHttpRequest();
-    xhr.addEventListener("load", function() {
+    xhr.addEventListener("load", function () {
         posts_data = JSON.parse(xhr.responseText)
-        //console.log(data)
         let last_post = document.getElementById("last_post")
         last_post.setAttribute("href", posts_data["published_post"]);
         // log("href: " + last_post.getAttribute("href")) //pisanie linku
@@ -99,29 +98,27 @@ function stuff_with_json_file(window_url) {
         if (window_url.path.startsWith("/stories")) {
             // log("stories");
             let value = 0;
-            if (window_url.search.has("date")){
+            if (window_url.search.has("date")) {
                 value += 1;
             }
             if (window_url.search.has("id"))
                 value += 2
-            
+
             switch (value) {
                 case 0: // nothing defined
                     // log("have nothing")
                     var list = []
                     var ul = document.createElement("ul")
-                    for(let key in posts_data["posts"])
+                    for (let key in posts_data["posts"])
                         list.push(key);
 
-                    for(let key in list.reverse()){
-                        console.log(list[key])
+                    for (let key in list.reverse()) {
                         key = list[key]
                         let a = document.createElement('a')
                         let li = document.createElement("li");
                         a.innerHTML = key;
-                        a.setAttribute("href", "?date="+key);
+                        a.setAttribute("href", "?date=" + key);
                         li.appendChild(a);
-                        //console.log(li)
                         ul.appendChild(li);
                     }
                     content.appendChild(ul)
@@ -133,29 +130,27 @@ function stuff_with_json_file(window_url) {
                     // log("date: "+date);
                     var list = []
                     var ul = document.createElement("ul")
-                    
+
                     let posts = posts_data["posts"][date];
-                    for(let i=posts.length-1; i>=0; i--){
+                    for (let i = posts.length - 1; i >= 0; i--) {
                         let post = posts[i];
-                        console.log(post)
                         let a = document.createElement('a')
                         let li = document.createElement("li");
                         a.innerHTML = post["title"];
-                        
-                        let temp_url =  new URLSearchParams(window_url.search)
+
+                        let temp_url = new URLSearchParams(window_url.search)
                         temp_url.append("id", i)
-                        a.setAttribute("href", "?"+temp_url.toString());
+                        a.setAttribute("href", "?" + temp_url.toString());
                         li.appendChild(a);
 
-                        a.classList.add("near-link","story-category");
+                        a.classList.add("near-link", "story-category");
                         a.dataset["category"] = categories[post["category"]];
-                        //console.log(li)
                         ul.appendChild(li)
                     }
 
 
                     content.appendChild(ul)
-                    change_title("Posty z: "+ date)
+                    change_title("Posty z: " + date)
                     break;
 
                 case 2: // only id defined
@@ -167,7 +162,6 @@ function stuff_with_json_file(window_url) {
                     let id = window_url.search.get("id")
                     let post = posts_data["posts"][date][id]
                     let text = post["content"].replaceAll("\n", "</p><p>")
-                    console.log(text)
 
                     let category = document.createElement("a")
                     category.classList.add("near-title", "story-category")
@@ -177,7 +171,7 @@ function stuff_with_json_file(window_url) {
                     change_title(post["title"])
                     break;
             }
-            if(value == 3) //jeśli jest wszystko podane
+            if (value == 3) //jeśli jest wszystko podane
                 content.classList.add("story-content");
             else
                 content.classList.add("story-links");
@@ -188,8 +182,13 @@ function stuff_with_json_file(window_url) {
     xhr.send(null)
 }
 
-function go_back_button_listener(){
-    console.log("Pressed go_back")
+function go_back_button_listener() {
+    if (document.referrer.startsWith(document.location.origin)) {
+        window.history.go(-1);
+    }
+    else {
+        window.location.pathname = "/";
+    }
 }
 
 
@@ -200,12 +199,10 @@ window.addEventListener("load", () => {
         document.getElementsByTagName("body")[0].appendChild(elem);
     } */
     let btn = this.document.getElementById("go_back");
-    btn.addEventListener("click", go_back_button_listener);
-    console.log(btn)
-    
+    if (btn)
+        btn.addEventListener("click", go_back_button_listener);
+
     var content = document.getElementById("content");
-    // log("Window loaded")
     let url_obj = slice_url();
-    console.log(url_obj)
     stuff_with_json_file(url_obj)
 })
